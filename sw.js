bdfb164d7822e66a734a6ff6aa348c1a3b9e97cb
kazuya2400364 <kazuya2400364@gmail.com>
2026-05-30 15:24:33 +0900

    Activate updated service worker immediately

--- sw.js
+++ sw.js
@@ -12,13 +12,14 @@ const APP_SHELL = [
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
       Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
-    )
+    ).then(() => self.clients.claim())
   );
 });
 
